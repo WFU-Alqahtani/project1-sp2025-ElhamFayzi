cb52b3376eeb2544e477541014f3c8545f4ec86d
Elham Ahmad Fayzi <fayze24@wfu.edu>
2025-01-30 17:08:43 -0500

    Made the exception-handling in createCart functional for any input.

--- src/Main.java
+++ src/Main.java
@@ -17,30 +17,34 @@ public class Main {
     public static ArrayList<Item> createCart(String[] args, Item[] store) {
         ArrayList<Item> cart = new ArrayList<Item>();
         int i = 0;
-
+        int n = 0;
         try {
-            int n = (args.length == 0) ? 0 : Integer.parseInt(args[0]);
-
-            if (args.length - 1 != n) {
-                if (args.length - 1 < n) {
-                    System.out.println("Not enough Input");
-                }
-                else {
-                    for (i = n + 1; i < args.length; i++) {
-                        System.out.println("The entered index \"" + args[i] + "\" is extra");
-                    }
-                }
-            }
-
-            for (i = 1; i < args.length; i++) {
-                cart.add(store[Integer.parseInt(args[i])]);
-            }
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("The store does not have an item of index " + args[i]);              // FIXME: Improve the message that would print out + Add the index that is not accessible
+            n = (args.length == 0) ? 0 : Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
-            System.out.println("\"" + args[i] + "\" is not a valid integer");                   // FIXME: Improve the message
+            System.out.println("Invalid Input");
         }
 
+        if (args.length - 1 != n) {
+            if (args.length - 1 < n) {
+                System.out.println("Not enough Input");
+            }
+            else {
+                for (i = n + 1; i < args.length; i++) {
+                    System.out.println("The entered index \"" + args[i] + "\" is extra");
+                }
+            }
+        }
+
+        for (i = 1; i < args.length; i++) {
+            try {
+                cart.add(store[Integer.parseInt(args[i])]);
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("The store does not have an item of index " + args[i]);
+            } catch (NumberFormatException e) {
+                System.out.println("\"" + args[i] + "\" is not a valid integer");
+            }
+
+        }
         return cart;
     }
 
